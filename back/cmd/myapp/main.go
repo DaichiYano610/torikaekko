aeec91b22d45b0d69f7d943030294845b72d56d0
Daichi Yano <y_di_ch@icloud.com>
2025-07-10 18:12:33 +0900

    初期化関数を呼び出し、データベースを初期化してからルーティングを登録するように修正

--- back/cmd/myapp/main.go
+++ back/cmd/myapp/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"myapp/internal/handler"
+	"myapp/internal/initializer"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	initializer.Env_load()
+	database := initializer.InitDB()
 	router := gin.Default()
-	handler.RegisterUserRoutes(router)
+	handler.RegisterUserRoutes(router, database)
 	router.Run(":8080")
 }
